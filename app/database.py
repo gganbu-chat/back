39e4edab8bfe7fd9f3eeff373c6070aa770a211a
sonjuhyeon <ths120521113105@daum.net>
2025-03-21 22:41:58 +0900

    database 수정

--- app/database.py
+++ app/database.py
@@ -9,7 +9,13 @@ import os
 load_dotenv()
 
 # PostgreSQL 연결 URL
-DATABASE_URL = os.getenv("DATABASE_URL")
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL=f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # SQLAlchemy 설정
 engine = create_engine(DATABASE_URL)
--- app/search.py
+++ app/search.py
@@ -18,7 +18,13 @@ from database import SessionLocal, Character, User
 load_dotenv()
 
 # 환경 변수 설정
-DATABASE_URL = os.getenv("DATABASE_URL")
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL=f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 SECRET_KEY = os.getenv("SECRET_KEY", "default_key")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
